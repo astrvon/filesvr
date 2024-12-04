ec7eaec97a85997bb296de98c269269c28c5e7ef
astrvon <abeldustin06@gmail.com>
2024-12-04 18:50:09 +0700

    Update: adding type of functions on middleware\auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,14 +1,18 @@
 import { Elysia, Context } from "elysia";
 import { verifyToken } from "../utils/jwt";
+import { JWTPayload } from "jose";
 
-export const authMiddleware = async (context: Context) => {
+export const authMiddleware = async (
+  context: Context,
+): Promise<{ user: JWTPayload }> => {
   console.log(context.request.headers["authorization"]);
-  const token = context.headers.authorization?.split(" ")[1];
+  const token: string | undefined =
+    context.headers.authorization?.split(" ")[1];
   if (!token) {
     throw new Error("No token provided");
   }
   try {
-    const user = await verifyToken(token);
+    const user: JWTPayload = await verifyToken(token);
     return { user };
   } catch (err) {
     context.set.status = 401;
@@ -16,7 +20,7 @@ export const authMiddleware = async (context: Context) => {
   }
 };
 
-export const adminMiddleware = (context: Context) => {
+export const adminMiddleware = (context: Context): void => {
   if (!context["user"].data.isAdmin) {
     context.set.status = 403;
     throw new Error("Admin access required");
